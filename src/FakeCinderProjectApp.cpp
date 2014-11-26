e5f2275877f92190690117759089a84939fa9ed1
wmbarr <wmbarr@gmail.com>
2014-11-26 14:05:38 -0500

    "Initial commit"

---
+++ src/FakeCinderProjectApp.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class FakeCinderProjectApp : public AppNative {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+};
+
+void FakeCinderProjectApp::setup()
+{
+}
+
+void FakeCinderProjectApp::mouseDown( MouseEvent event )
+{
+}
+
+void FakeCinderProjectApp::update()
+{
+}
+
+void FakeCinderProjectApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP_NATIVE( FakeCinderProjectApp, RendererGl )
