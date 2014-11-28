770e30d54a025d75867b91dca013a8d92cd1a5a6
wmbarr <wmbarr@gmail.com>
2014-11-27 20:26:24 -0500

    latest

--- src/FakeCinderProjectApp.cpp
+++ src/FakeCinderProjectApp.cpp
@@ -1,5 +1,7 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+//#include "cinder/ImageIo.h"
+//#include "cinder/gl/Texture.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -11,15 +13,30 @@ class FakeCinderProjectApp : public AppNative {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+    void prepareSettings ( Settings *settings );
+  //  gl::Texture myImage;
+    
 };
+
+void FakeCinderProjectApp::prepareSettings( Settings *settings )
+{
+//    settings->setWindowSize( 800, 600 );
+ //   settings->setFrameRate( 60.0f );
+}
 
 void FakeCinderProjectApp::setup()
 {
+ //   myImage = gl::Texture(loadImage(loadResource("chester01.jpg")));
+    
+    
+
 }
+
+
 
 void FakeCinderProjectApp::mouseDown( MouseEvent event )
 {
-     cout << "Hello World! Again!!";
+  
 }
 
 void FakeCinderProjectApp::update()
@@ -27,9 +44,20 @@ void FakeCinderProjectApp::update()
 }
 
 void FakeCinderProjectApp::draw()
-{
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+{   // float gray = sin( getElapsedSeconds() ) * 0.5f + 0.5f;
+    // gl::clear( Color( gray,gray, gray), true );
+    // clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) );
+
+//gl::draw( myImage, getWindowBounds() );
+
+//    gl::drawSolidCircle( Vec2f( 15.0f, 25.0f ), 50.0f );
+ 
+    float x = cos(getElapsedSeconds())*100.0f;
+    float y = sin(getElapsedSeconds())*100.0f;
+    gl::drawSolidCircle( Vec2f( x+getWindowWidth()/2, y+getWindowHeight()/2 ), 50.0f);
 }
+
+
 
 CINDER_APP_NATIVE( FakeCinderProjectApp, RendererGl )
